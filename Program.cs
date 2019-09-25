1d518faf2e13d0682fb037647b8449c967163bf6
Craig Mellon <craig@etech.net>
2019-09-25 12:34:26 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace efcore_cosmosdb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
